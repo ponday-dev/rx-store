89da7fb1764fd5862cdeccef7b13e3c439a092bf
Honda <honda.y@jp.fujitsu.com>
2017-09-05 11:22:32 +0900

    [update] ビルド構成をライブラリ向けに変更

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const baseConfig = require('./webpack.config.base');
 
+const libraryName = 'rx-store';
+
 const devCofig = {
-    entry: {
-        app: path.resolve(__dirname, '../src/lib/index.ts')
-    },
+    entry: path.resolve(__dirname, '../src/lib/rx-store.ts'),
     output: {
         path: path.join(__dirname, '..', 'dist', 'lib'),
-        filename: 'rx-store.js'
+        filename: `${libraryName}.js`,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
 };
 
