492cde6f67298ddbfb3ca8f4d33f9e97379f57b6
ponday-dev <ponday.dev+github@gmail.com>
2018-05-13 16:18:26 +0900

    [update] RxJS 6.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { map, distinctUntilChanged } from 'rxjs/operators';
 
 type Reduce<T> = (state: T, opt?: any) => T;
